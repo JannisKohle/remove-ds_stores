d9602ff18b221efb7d638bdb53ebac17b3929f3a
JannisKohle <48388575+JannisKohle@users.noreply.github.com>
2020-04-21 14:20:38 +0200

    created some variables

--- code.py
+++ code.py
@@ -1,7 +1,11 @@
 import os
 
+path = "../.."  # CHANGE THIS
+
+see_noEnding_message = True  # CHANGE THIS
+
 file_endings = [".txt", ".json", ".py", ".html", ".css",
-                ".js", ".md", ".csv", ".c", ".cpp", ".pyc", ".swift", ".plaintext", ".png", ".jpg", "config", ".git", ".tif", ".iml", ".xml", ".egg", ".pth", ".cfg", ".csh"]  # all file endings you are using
+                ".js", ".md", ".csv", ".c", ".cpp", ".pyc", ".swift", ".plaintext", ".png", ".jpg", "config", ".git", ".tif", ".iml", ".xml", ".egg", ".pth", ".cfg", ".csh"]  # CHANGE THIS
 
 
 def not_ends_with_one(s, l):
@@ -13,10 +17,12 @@ def not_ends_with_one(s, l):
 
 
 def rm_here():
+    global file_endings
+    global see_noEnding_message
+
     if ".DS_Store" in os.listdir("."):
         os.system("rm -rf .DS_Store")
 
-    global file_endings
     folders = [i for i in os.listdir(".") if not_ends_with_one(i, file_endings)]  # get all folders
 
     for i in folders:
@@ -24,11 +30,14 @@ def rm_here():
             os.chdir(i)
             rm_here()
         except NotADirectoryError:  # i is a file
-            print(f"{os.getcwd()}/{i} is a file without an ending")
+            if see_noEnding_message:
+                print(f"{os.getcwd()}/{i} is a file without an ending")
+            else:
+                pass
 
     os.chdir("..")
 
 
-os.chdir("../..")  # go to my projects folders
+os.chdir(path)  # go to my projects folders
 
 rm_here()
